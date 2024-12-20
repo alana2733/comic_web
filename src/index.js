d8d75b6b904657c8c6f2bd979808c04c908cf70d
Phuong Lan <164603262+alana2733@users.noreply.github.com>
2024-12-20 18:47:37 +0700

    comicweb

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from "react-query"; // Import QueryClient và QueryClientProvider
 
+// Tạo một QueryClient
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
